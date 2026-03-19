b1a63a759d886aa72634d24804466970726465a8
omnia771148 <omnia771148@gmail.com>
2026-03-19 23:54:34 +0530

    initial commit

--- src/app/data/page.js
+++ src/app/data/page.js
@@ -1648,7 +1648,7 @@ export const Data = [
 
 //family packs
 
-{ id: 1069, name: "Family pack C.B(2)+Pulks(6)+Chicken 65+Chicken Curry+curd rice(1)", price: 15, restid: 10, button: "Add", type: "non-veg", symbol: "🔺", image: "/fooditems/1 (200).webp", category: "FAMILY PACKS" },
+{ id: 1069, name: "Family pack C.B(2)+Pulks(6)+Chicken 65+Chicken Curry+curd rice(1)", price: 900, restid: 10, button: "Add", type: "non-veg", symbol: "🔺", image: "/fooditems/1 (200).webp", category: "FAMILY PACKS" },
 
 
 
